a4e4fc808d317cad341aa5cb346d073c4669cac6
Sarthak Kad <sarthakkad15@gmail.com>
2026-07-17 23:18:36 +0530

    Time: 0 ms (100%), Space: 23.8 MB (45.61%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,12 +3,43 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
         vector<int> ans;
-        map<int,int> m;
+        int n1=0,n2=0,c1=0,c2=0,x;
         for(int i=0;i<n;i++)
         {
-            if((++m[nums[i]] > n/3) && find(ans.begin(),ans.end(),nums[i])==ans.end())
-                ans.push_back(nums[i]);
+            x=nums[i];
+            if(x==n1)
+                c1++;
+            else if(x==n2)
+                c2++;
+            else if(c1==0)
+            {
+                n1=x;
+                c1=1;
+            }
+            else if(c2==0)
+            {
+                n2=x;
+                c2=1;
+            }
+            else
+            {
+                c1--;
+                c2--;
+            }
         }
+        c1=0;
+        c2=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==n1)
+                c1++;
+            else if(nums[i]==n2)
+                c2++;
+        }
+        if(c1>n/3)
+            ans.push_back(n1);
+        if(c2>n/3)
+            ans.push_back(n2);
         return ans;
     }
 };
