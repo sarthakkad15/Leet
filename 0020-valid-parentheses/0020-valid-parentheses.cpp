7223f0fbdd9a54820e636584402c2ca2693c64ea
Sarthak Kad <sarthakkad15@gmail.com>
2026-08-18 14:35:23 +0530

    Time: 0 ms (100%), Space: 9 MB (37.85%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n=s.length();
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+                st.push(s[i]);
+            else if(st.empty())
+                return false;
+            else if(s[i]==')' && st.top()!='(')
+                return false;
+            else if(s[i]=='}' && st.top()!='{')
+                return false;
+            else if(s[i]==']' && st.top()!='[')
+                return false;
+            else
+                st.pop();
+        }
+        if(st.empty())
+            return true;
+        return false;
+    }
+};
