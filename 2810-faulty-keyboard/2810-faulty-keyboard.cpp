59194ef38e254215a3d85d9c8c0ad368e3d2886b
Sarthak Kad <sarthakkad15@gmail.com>
2026-09-02 13:57:57 +0530

    Time: 0 ms (100%), Space: 9.2 MB (85.6%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string ans="";
+        for(char c:s)
+        {
+            if(c=='i')
+            {
+                reverse(ans.begin(),ans.end());
+                continue;
+            }
+            ans.push_back(c);
+        }
+        return ans;
+    }
+};
